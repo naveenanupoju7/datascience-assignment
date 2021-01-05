e564033add2465e62e94f95fc60aa27f28f20247
naveenanupoju7 <72677009+naveenanupoju7@users.noreply.github.com>
2021-01-05 09:37:55 +0530

    Add files via upload

--- plotly check.py
+++ plotly check.py
@@ -2,10 +2,9 @@ import plotly.express as px
 import pandas as pd
   
 df=pd.read_excel(r'G:\TROP ICSU INTERNSHIP\India-Temp-2011.xlsx',usecols=['ANNUAL','JAN-FEB','MAR-MAY','JUN-SEP','OCT-DEC'],engine='openpyxl')
-df.shape
+
 print(df.describe())
-print(df.quality.unique())
-#df.quality.value_counts()
+
 fig = px.box(df,points="all",title="Boxplot ",labels={"value":"Temperature(°C)","variable":"Time range"})
 
 fig.show()
